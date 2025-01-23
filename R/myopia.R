964041d68ed9bf7e22cc662dd13b758ede511e6a
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-01-23 14:34:47 -0800

    more

---
+++ R/myopia.R
@@ -0,0 +1,12 @@
+#' Myopia data
+#'
+#' Data on myopia from Quinn et al
+#'
+#' @format ## `myopia`
+#' A data frame with 479 rows and 2
+#' \describe{
+#'   \item{Light}{Level of night light}
+#'   \item{Sight}{Level of myopia}
+#' }
+#' @source <https://math.montana.edu/courses/s216/data/ChildrenLightSight.csv>
+"myopia"
--- data-raw/nightlight.R
+++ data-raw/nightlight.R
@@ -1,5 +1,7 @@
 ## code to prepare `nightlight` dataset goes here
 
-myopia <- readr::read_csv("https://math.montana.edu/courses/s216/data/ChildrenLightSight.csv")
+# from https://math.montana.edu/courses/s216/data/ChildrenLightSight.csv
+
+myopia <- readr::read_csv("inst/extdata/ChildrenLightSight.csv")
 
 usethis::use_data(myopia, overwrite = TRUE)
